7a2ce7a49330479cc2edafe0e6b76e6674cfe435
riggs <gabriel.p.riggs@gmail.com>
2025-09-02 16:37:28 -0600

    fixed incrementing

--- api/seeds/01_users.js
+++ api/seeds/01_users.js
@@ -12,9 +12,13 @@ exports.seed = async function(knex) {
   const hashedPassword2 = await bcrypt.hash('password2',12);
   const hashedPassword3 = await bcrypt.hash('password3',12);
 
+
   await knex('users').insert([
     {id: 1, first_name: 'john', last_name: 'doe', username: 'user1', password: hashedPassword1},
     {id: 2, first_name: 'josh', last_name: 'does', username: 'user2', password: hashedPassword2},
     {id: 3, first_name: 'jacob', last_name: 'los', username: 'user3', password: hashedPassword3}
   ]);
+
+  
+  await knex.raw(`SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));`)
 };
